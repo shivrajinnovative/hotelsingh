0307433d06d1e16c45399cbe603f4e473430f1f4
shivrajinnovative <shivraj.innovative@gmail.com>
2024-07-11 17:32:10 +0530

    home page styling 60% complete

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+const navbar=document.querySelector(".navbar")
 window.onscroll = function() {
-    const navbar=document.querySelector(".navbar")
+
+    if(window.innerWidth > 600){
+
+
     if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
         navbar.style.backgroundColor="black"
     }else{
         navbar.style.backgroundColor='transparent'
-
     }
+}
 }
